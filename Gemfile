a1359b2efce7a8c4c2250dbfc8ecceb4110eed70
Trung <nguyenvantrung2015@gmail.com>
2019-07-31 00:26:00 +0700

    Heroku config

--- Gemfile
+++ Gemfile
@@ -21,7 +21,7 @@ gem "rubocop", "~> 0.54.0", require: false
 gem "sass-rails", "~> 5.0"
 gem "sprockets-rails"
 gem "turbolinks", "~> 5"
-gem "uglifier", ">= 1.3.0"
+gem "uglifier"
 group :development, :test do
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
   gem "sqlite3"
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2019_07_26_013627) do
 
-  create_table "celebrities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "celebrities", force: :cascade do |t|
     t.string "name"
     t.datetime "date_of_birth"
     t.integer "gender"
@@ -22,36 +22,36 @@ ActiveRecord::Schema.define(version: 2019_07_26_013627) do
     t.string "portrait"
   end
 
-  create_table "celebrity_media", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "celebrity_media", force: :cascade do |t|
     t.integer "role"
-    t.bigint "celebrity_id"
-    t.bigint "medium_id"
+    t.integer "celebrity_id"
+    t.integer "medium_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["celebrity_id"], name: "index_celebrity_media_on_celebrity_id"
     t.index ["medium_id"], name: "index_celebrity_media_on_medium_id"
   end
 
-  create_table "episodes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "episodes", force: :cascade do |t|
     t.datetime "release_date"
     t.float "critic_score"
     t.float "audience_score"
     t.text "info"
-    t.bigint "season_id"
+    t.integer "season_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "episode_number"
     t.index ["season_id"], name: "index_episodes_on_season_id"
   end
 
-  create_table "media", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "media", force: :cascade do |t|
     t.string "reviewable_type"
     t.integer "reviewable_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "movies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "movies", force: :cascade do |t|
     t.string "name"
     t.datetime "release_date"
     t.float "critic_score"
@@ -62,28 +62,28 @@ ActiveRecord::Schema.define(version: 2019_07_26_013627) do
     t.string "poster"
   end
 
-  create_table "news", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "news", force: :cascade do |t|
     t.text "content"
-    t.bigint "user_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_news_on_user_id"
   end
 
-  create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "reviews", force: :cascade do |t|
     t.text "content"
     t.integer "score"
-    t.bigint "user_id"
-    t.bigint "medium_id"
+    t.integer "user_id"
+    t.integer "medium_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["medium_id"], name: "index_reviews_on_medium_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
-  create_table "seasons", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "seasons", force: :cascade do |t|
     t.text "info"
-    t.bigint "tv_show_id"
+    t.integer "tv_show_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "poster"
@@ -91,7 +91,7 @@ ActiveRecord::Schema.define(version: 2019_07_26_013627) do
     t.index ["tv_show_id"], name: "index_seasons_on_tv_show_id"
   end
 
-  create_table "tv_shows", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "tv_shows", force: :cascade do |t|
     t.string "name"
     t.text "info"
     t.datetime "created_at", null: false
@@ -99,7 +99,7 @@ ActiveRecord::Schema.define(version: 2019_07_26_013627) do
     t.string "poster"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "organization"
@@ -111,11 +111,4 @@ ActiveRecord::Schema.define(version: 2019_07_26_013627) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  add_foreign_key "celebrity_media", "celebrities"
-  add_foreign_key "celebrity_media", "media"
-  add_foreign_key "episodes", "seasons"
-  add_foreign_key "news", "users"
-  add_foreign_key "reviews", "media"
-  add_foreign_key "reviews", "users"
-  add_foreign_key "seasons", "tv_shows"
 end
